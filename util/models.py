1b889e83acf804eae8acbe339e589e10fbacbb33
Masaru <c1220478@st.kanazawa-it.ac.jp>
2025-04-28 20:47:37 +0900

    feat: 結果評価のデータクラスを定義

--- util/models.py
+++ util/models.py
@@ -15,8 +15,20 @@ class Dataset:
     item_content: pd.DataFrame
 
 @dataclasses.dataclass(frozen=True)
+# 推薦システムの予測結果
 class RecommendResult:
     # テストデータセットの予測評価値。RSMEの評価
     rating: pd.DataFrame
     # キーはユーザーID, バリューはおすすめアイテムのIDのリスト。ランキング指標の評価
     user2items: Dict[int, List[int]]
+
+@dataclasses.dataclass(frozen=True)
+#推薦システムの評価
+class Metrics:
+    rmse: float
+    precision_at_k: float
+    recall_at_k: float
+
+    # 評価結果を出力するときに少数は第3桁までにする
+    def __repr__(self):
+        return f"rmse={self.rmse:.3f}, Precision@K={self.precision_at_k:.3f}, Recall@K={self.recall_at_k:.3f}"
